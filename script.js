7c933331f348851bd16ee323dd2e25fc6ba3b04e
FurkanSD <FSDeryol@gmail.com>
2022-08-14 00:46:52 +0300

    button animations added

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const showMoreCustomButton = document.querySelector(".show-more-btn-custom")
 const showLessCustomButton = document.querySelector(".show-less-btn-custom")
 const randomShortcutsSection = document.querySelector("[data-custom-shortcuts]")
 const customEditButton = document.querySelector(".edit")
+const emotionsContainer = document.querySelector(".emotions-container")
+const likeButton = document.querySelector(".reaction-like")
+const reactionLikeBtn = document.querySelector(".reaction-btn-like")
+const emojis = document.querySelectorAll(".emoji")
 
 hideDefaultShortcuts.forEach(shortcut => {
   shortcut.classList.add("hide")
@@ -53,4 +57,85 @@ randomShortcutsSection.addEventListener("mouseover", () => {
 
 randomShortcutsSection.addEventListener("mouseleave", () => {
   customEditButton.classList.add("hide")
+})
+
+likeButton.addEventListener("mouseenter", () => {
+  emotionsContainer.classList.remove("hide")
+  emotionsContainer.style.transform = "translateY'(-10%)'"
+})
+
+likeButton.addEventListener("mouseleave", () => {
+  emotionsContainer.classList.add("hide")
+})
+
+emotionsContainer.addEventListener("mouseenter", () => {
+  emotionsContainer.classList.remove("hide")
+})
+
+emotionsContainer.addEventListener("mouseleave", () => {
+  emotionsContainer.classList.remove("hide")
+})
+
+likeButton.addEventListener("click", () => {
+  reactionLikeBtn.removeAttribute("class")
+  emotionsContainer.classList.add("hide")
+  reactionLikeBtn.classList.add("reaction-btn")
+  window.requestAnimationFrame(function () {
+    reactionLikeBtn.classList.add("like-btn-like")
+  })
+})
+
+emojis.forEach(emoji => {
+  emoji.addEventListener("click", e => {
+    if (e.target.matches("[data-reaction-like]")) {
+      reactionLikeBtn.removeAttribute("class")
+      emotionsContainer.classList.add("hide")
+      reactionLikeBtn.classList.add("reaction-btn")
+      window.requestAnimationFrame(function () {
+        reactionLikeBtn.classList.add("like-btn-like")
+      })
+    } else if (e.target.matches("[data-reaction-love]")) {
+      reactionLikeBtn.removeAttribute("class")
+      emotionsContainer.classList.add("hide")
+      reactionLikeBtn.classList.add("reaction-btn")
+      window.requestAnimationFrame(function () {
+        reactionLikeBtn.classList.add("like-btn-love")
+      })
+    } else if (e.target.matches("[data-reaction-care")) {
+      reactionLikeBtn.removeAttribute("class")
+      emotionsContainer.classList.add("hide")
+      reactionLikeBtn.classList.add("reaction-btn")
+      window.requestAnimationFrame(function () {
+        reactionLikeBtn.classList.add("like-btn-care")
+      })
+    } else if (e.target.matches("[data-reaction-smile")) {
+      reactionLikeBtn.removeAttribute("class")
+      emotionsContainer.classList.add("hide")
+      reactionLikeBtn.classList.add("reaction-btn")
+      window.requestAnimationFrame(function () {
+        reactionLikeBtn.classList.add("like-btn-smile")
+      })
+    } else if (e.target.matches("[data-reaction-wow")) {
+      reactionLikeBtn.removeAttribute("class")
+      emotionsContainer.classList.add("hide")
+      reactionLikeBtn.classList.add("reaction-btn")
+      window.requestAnimationFrame(function () {
+        reactionLikeBtn.classList.add("like-btn-wow")
+      })
+    } else if (e.target.matches("[data-reaction-sad")) {
+      reactionLikeBtn.removeAttribute("class")
+      emotionsContainer.classList.add("hide")
+      reactionLikeBtn.classList.add("reaction-btn")
+      window.requestAnimationFrame(function () {
+        reactionLikeBtn.classList.add("like-btn-sad")
+      })
+    } else if (e.target.matches("[data-reaction-angry")) {
+      reactionLikeBtn.removeAttribute("class")
+      emotionsContainer.classList.add("hide")
+      reactionLikeBtn.classList.add("reaction-btn")
+      window.requestAnimationFrame(function () {
+        reactionLikeBtn.classList.add("like-btn-angry")
+      })
+    }
+  })
 })
