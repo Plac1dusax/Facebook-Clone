40b2068df8681fe1abba22072c13d44c6e8571c2
FurkanSD <FSDeryol@gmail.com>
2022-08-18 02:04:33 +0300

    json file added

--- script.js
+++ script.js
@@ -7,9 +7,180 @@ const showLessCustomButton = document.querySelector(".show-less-btn-custom")
 const randomShortcutsSection = document.querySelector("[data-custom-shortcuts]")
 const customEditButton = document.querySelector(".edit")
 const emotionsContainer = document.querySelector(".emotions-container")
+const reactions = document.querySelector(".reactions")
+const communityReaction = document.querySelectorAll(".community-reaction")
 const likeButton = document.querySelector(".reaction-like")
-const reactionLikeBtn = document.querySelector(".reaction-btn-like")
+const reactionLikeBtn = document.querySelector(".reaction-emoji-btn")
+const likeBtnText = document.querySelector(".like-btn-text")
+let likeCounter = parseInt(document.querySelector(".like-counter").textContent)
+const totalLikes = document.querySelector(".like-counter")
 const emojis = document.querySelectorAll(".emoji")
+const posts = document.querySelectorAll(".post")
+
+for (let i = 0; i < posts.length; i++) {
+  posts[i].dataset.id = i
+}
+
+async function getLikes() {
+  let response = await fetch("posts.json")
+
+  const likes = await response.json()
+
+  return likes
+}
+async function renderLikes() {
+  let likes = await getLikes()
+
+  for (let i = 0; i < posts.length; i++) {
+    posts.forEach(post => {
+      if (post.dataset.id == likes[i].id) {
+        if (likes[i].like !== 0) {
+          const reaction = post.querySelector(".community-reaction-like")
+          reaction.classList.remove("hide")
+        } else {
+          const reaction = post.querySelector(".community-reaction-like")
+          reaction.classList.add("hide")
+        }
+        if (likes[i].love !== 0) {
+          const reaction = post.querySelector(".community-reaction-love")
+          reaction.classList.remove("hide")
+        } else {
+          const reaction = post.querySelector(".community-reaction-love")
+          reaction.classList.add("hide")
+        }
+        if (likes[i].care !== 0) {
+          const reaction = post.querySelector(".community-reaction-care")
+          reaction.classList.remove("hide")
+        } else {
+          const reaction = post.querySelector(".community-reaction-care")
+          reaction.classList.add("hide")
+        }
+        if (likes[i].smile !== 0) {
+          const reaction = post.querySelector(".community-reaction-smile")
+          reaction.classList.remove("hide")
+        } else {
+          const reaction = post.querySelector(".community-reaction-smile")
+          reaction.classList.add("hide")
+        }
+        if (likes[i].wow !== 0) {
+          const reaction = post.querySelector(".community-reaction-wow")
+          reaction.classList.remove("hide")
+        } else {
+          const reaction = post.querySelector(".community-reaction-wow")
+          reaction.classList.add("hide")
+        }
+        if (likes[i].sad !== 0) {
+          const reaction = post.querySelector(".community-reaction-sad")
+          reaction.classList.remove("hide")
+        } else {
+          const reaction = post.querySelector(".community-reaction-sad")
+          reaction.classList.add("hide")
+        }
+        if (likes[i].angry !== 0) {
+          const reaction = post.querySelector(".community-reaction-angry")
+          reaction.classList.remove("hide")
+        } else {
+          const reaction = post.querySelector(".community-reaction-angry")
+          reaction.classList.add("hide")
+        }
+      }
+    })
+  }
+}
+
+async function handleLike() {
+  let likes = await getLikes()
+
+  emojis.forEach(emoji => {
+    emoji.addEventListener("click", e => {
+      addAnimation(
+        e,
+        "[data-reaction-like]",
+        "like",
+        "Like",
+        "blue",
+        "like-btn-like",
+        likes
+      )
+      addAnimation(
+        e,
+        "[data-reaction-love]",
+        "love",
+        "Love",
+        "purple",
+        "like-btn-love",
+        likes
+      )
+      addAnimation(
+        e,
+        "[data-reaction-care]",
+        "care",
+        "Care",
+        "orange",
+        "like-btn-care",
+        likes
+      )
+      addAnimation(
+        e,
+        "[data-reaction-smile]",
+        "smile",
+        "Smile",
+        "orange",
+        "like-btn-smile",
+        likes
+      )
+      addAnimation(
+        e,
+        "[data-reaction-wow]",
+        "wow",
+        "Wow",
+        "orange",
+        "like-btn-wow",
+        likes
+      )
+      addAnimation(
+        e,
+        "[data-reaction-sad]",
+        "sad",
+        "Sad",
+        "orange",
+        "like-btn-sad",
+        likes
+      )
+      addAnimation(
+        e,
+        "[data-reaction-angry]",
+        "angry",
+        "Angry",
+        "red",
+        "like-btn-angry",
+        likes
+      )
+    })
+  })
+}
+
+handleLike()
+renderLikes()
+
+async function logLikes() {
+  let likes = await getLikes()
+  console.log(likes)
+}
+
+logLikes()
+function addAnimation(e, dataAttribute, emoji, text, color, className, likes) {
+  if (e.target.matches(dataAttribute)) {
+    let postId =
+      e.target.parentElement.parentElement.parentElement.parentElement
+        .parentElement.dataset.id
+
+    let total = likes[postId][emoji]
+    likes[postId][emoji] = total + 1
+    console.log(likes[postId][emoji])
+    playAnimation(text, color, className)
+  }
+}
 
 hideDefaultShortcuts.forEach(shortcut => {
   shortcut.classList.add("hide")
@@ -76,66 +247,14 @@ emotionsContainer.addEventListener("mouseleave", () => {
   emotionsContainer.classList.remove("hide")
 })
 
-likeButton.addEventListener("click", () => {
+function playAnimation(text, color, animation) {
+  likeBtnText.textContent = text
+  likeBtnText.style.color = color
+
   reactionLikeBtn.removeAttribute("class")
   emotionsContainer.classList.add("hide")
-  reactionLikeBtn.classList.add("reaction-btn")
+  reactionLikeBtn.classList.add("reaction-emoji-btn")
   window.requestAnimationFrame(function () {
-    reactionLikeBtn.classList.add("like-btn-like")
+    reactionLikeBtn.classList.add(animation)
   })
-})
-
-emojis.forEach(emoji => {
-  emoji.addEventListener("click", e => {
-    if (e.target.matches("[data-reaction-like]")) {
-      reactionLikeBtn.removeAttribute("class")
-      emotionsContainer.classList.add("hide")
-      reactionLikeBtn.classList.add("reaction-btn")
-      window.requestAnimationFrame(function () {
-        reactionLikeBtn.classList.add("like-btn-like")
-      })
-    } else if (e.target.matches("[data-reaction-love]")) {
-      reactionLikeBtn.removeAttribute("class")
-      emotionsContainer.classList.add("hide")
-      reactionLikeBtn.classList.add("reaction-btn")
-      window.requestAnimationFrame(function () {
-        reactionLikeBtn.classList.add("like-btn-love")
-      })
-    } else if (e.target.matches("[data-reaction-care")) {
-      reactionLikeBtn.removeAttribute("class")
-      emotionsContainer.classList.add("hide")
-      reactionLikeBtn.classList.add("reaction-btn")
-      window.requestAnimationFrame(function () {
-        reactionLikeBtn.classList.add("like-btn-care")
-      })
-    } else if (e.target.matches("[data-reaction-smile")) {
-      reactionLikeBtn.removeAttribute("class")
-      emotionsContainer.classList.add("hide")
-      reactionLikeBtn.classList.add("reaction-btn")
-      window.requestAnimationFrame(function () {
-        reactionLikeBtn.classList.add("like-btn-smile")
-      })
-    } else if (e.target.matches("[data-reaction-wow")) {
-      reactionLikeBtn.removeAttribute("class")
-      emotionsContainer.classList.add("hide")
-      reactionLikeBtn.classList.add("reaction-btn")
-      window.requestAnimationFrame(function () {
-        reactionLikeBtn.classList.add("like-btn-wow")
-      })
-    } else if (e.target.matches("[data-reaction-sad")) {
-      reactionLikeBtn.removeAttribute("class")
-      emotionsContainer.classList.add("hide")
-      reactionLikeBtn.classList.add("reaction-btn")
-      window.requestAnimationFrame(function () {
-        reactionLikeBtn.classList.add("like-btn-sad")
-      })
-    } else if (e.target.matches("[data-reaction-angry")) {
-      reactionLikeBtn.removeAttribute("class")
-      emotionsContainer.classList.add("hide")
-      reactionLikeBtn.classList.add("reaction-btn")
-      window.requestAnimationFrame(function () {
-        reactionLikeBtn.classList.add("like-btn-angry")
-      })
-    }
-  })
-})
+}
